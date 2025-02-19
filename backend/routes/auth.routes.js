de19e6c94a2c35c46ef5f540610f8cd5893f123f
Sai Sushruth Pothireddy <user.name>
2025-02-19 22:13:40 +0530

    1- user routes and controllers added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMe, login, logout, signup } from "../controllers/auth.controllers.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
